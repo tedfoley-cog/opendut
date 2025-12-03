a1d42e82de80dadeea9d80182716afeb8431161f
Paul Krotofil <paul.krotofil@mercedes-benz.com>
2025-12-03 16:32:45 +0100

    cleanUp script

--- .ci/deploy/localenv/data/provision/scripts/generate-client-certificate.sh
+++ .ci/deploy/localenv/data/provision/scripts/generate-client-certificate.sh
@@ -9,18 +9,14 @@ OPENDUT_PASSWORD_FILE="$PROVISION_ROOT_DIR/.env-pki"
 OPENDUT_ENV_FILE="$PROVISION_ROOT_DIR/.env"
 CA_PATH="$PROVISION_PKI_DIR/$OPENDUT_CERT_CA_NAME"
 
-# --- Additional: Generate a client certificate signed by the same CA ---
-# Only generate if CA files exist; name derived from CA basename
 CA_BASENAME="$(basename "$CA_PATH")"
 if [[ -f "$CA_PATH.key" && -f "$CA_PATH.pem" ]]; then
   CLIENT_NAME="${CA_BASENAME}-auth-client"
   CLIENT_CERT_PATH="$PROVISION_PKI_DIR/$CLIENT_NAME"
   CLIENT_DEPLOY_PATH="$PROVISION_PKI_DIR/deploy/$CLIENT_NAME"
 
-# Client CSR + key
   openssl req -new -sha512 -passout file:"$OPENDUT_PASSWORD_FILE" -out "$CLIENT_CERT_PATH".csr -newkey rsa:4096 -keyout "$CLIENT_CERT_PATH".key -subj "/CN=$CLIENT_NAME/C=XX/ST=Some-State/O=ExampleOrg"
 
-# Client v3 ext with EKU clientAuth
   cat > "$CLIENT_CERT_PATH".v3.ext << EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
@@ -31,21 +27,18 @@ subjectAltName = @alt_names
 DNS.1 = $CLIENT_NAME
 EOF
 
-# Sign client certificate (reuse serial file if present)
-  SERIAL_FILE="$CA_PATH".srl
-  if [ -f "$SERIAL_FILE" ]; then
+
+SERIAL_FILE="$CA_PATH".srl
+ if [ -f "$SERIAL_FILE" ]; then
     openssl x509 -req -in "$CLIENT_CERT_PATH".csr -CA "$CA_PATH".pem -CAkey "$CA_PATH".key -passin file:"$OPENDUT_PASSWORD_FILE" -CAserial "$SERIAL_FILE" -outform PEM -out "$CLIENT_CERT_PATH".pem -days 9999 -sha256 -extfile "$CLIENT_CERT_PATH".v3.ext
-  else
+ else
     openssl x509 -req -in "$CLIENT_CERT_PATH".csr -CA "$CA_PATH".pem -CAkey "$CA_PATH".key -passin file:"$OPENDUT_PASSWORD_FILE" -CAcreateserial -outform PEM -out "$CLIENT_CERT_PATH".pem -days 9999 -sha256 -extfile "$CLIENT_CERT_PATH".v3.ext
-  fi
+ fi
 
-# Deploy client cert and decrypted key
-  cp "$CLIENT_CERT_PATH".pem "$CLIENT_DEPLOY_PATH".pem
-  openssl rsa -in "$CLIENT_CERT_PATH".key -passin file:"$OPENDUT_PASSWORD_FILE" -out "$CLIENT_DEPLOY_PATH".key
+cp "$CLIENT_CERT_PATH".pem "$CLIENT_DEPLOY_PATH".pem
+openssl rsa -in "$CLIENT_CERT_PATH".key -passin file:"$OPENDUT_PASSWORD_FILE" -out "$CLIENT_DEPLOY_PATH".key
 
-# Also provide client certificate as .crt (PEM format)
-  openssl x509 -in "$CLIENT_CERT_PATH".pem -out "$CLIENT_DEPLOY_PATH".crt -outform PEM
+openssl x509 -in "$CLIENT_CERT_PATH".pem -out "$CLIENT_DEPLOY_PATH".crt -outform PEM
 
-# Cleanup client CSR/ext
-  rm "$CLIENT_CERT_PATH".csr
-  rm "$CLIENT_CERT_PATH".v3.ext
+rm "$CLIENT_CERT_PATH".csr
+rm "$CLIENT_CERT_PATH".v3.ext
